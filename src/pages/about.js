0314e07f9ce3abaeb0ca3b5fd44598ff35b3997a
Jeffrey <jeffreylee@jeffreys-mbp.lan>
2022-07-20 08:48:31 -0700

    Testing styled components

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,9 @@ import Layout from '../components/Layout';
 const about = () => {
   return (
     <Layout>
-      <h1>about page</h1>
+      <div className='about-heading'>
+        <h1>about page</h1>
+      </div>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from 'gatsby';
 import Layout from "../components/Layout";
+import { page, text } from "../examples/home.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <h1 style={{ color: "green", textTransform: "capitalize" }}>Hello world!</h1>
-      
-      <h1>hello world</h1>
+      <div className={page}>
+        <h1>Hello world!</h1>
+        
+        <h1>hello world</h1>
 
-      <h1>hello people</h1>
+        <h1>hello people</h1>
+
+        <p className={text}>lorem-ipsum.
+        </p>
+      </div>
     </Layout>
     )
 }
