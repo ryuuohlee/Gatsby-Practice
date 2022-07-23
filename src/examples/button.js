90f0eac9acac3c5146e0bb2eadd5c69788270beb
Jeffrey <jeffreylee@jeffreys-mbp.lan>
2022-07-22 21:07:51 -0700

    Testing styled components

---
+++ src/examples/button.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import React from 'react'
+
+export const ExampleButton = styled.button`
+    background-color: green;
+    color: orange;
+    font-size: 2rem;
+`
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import Layout from '../components/Layout';
+import styled from 'styled-components';
 
 const about = () => {
   return (
     <Layout>
-      <div className='about-heading'>
+      <Wrapper>
         <h1>about page</h1>
-      </div>
+        <p className="text">Lorem ipsum.</p>
+      </Wrapper>
     </Layout>
   )
 }
+
+const Wrapper = styled.section`
+  color: blue;
+  h1 {
+    color: yellowgreen;
+  }
+  
+  text {
+    text-transform: uppercase;
+  }
+`
 
 export default about;
