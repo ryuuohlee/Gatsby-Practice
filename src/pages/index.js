41312fd9006967fa2d5edafb87c9384e252f97de
Jeffrey <jglee1@live.com>
2022-08-06 11:26:58 -0700

    Working on home page hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { Link } from 'gatsby';
 import Layout from "../components/Layout";
-import Images from '../examples/images.js';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function Home() {
   return (
     <Layout>
-      <h1>home page</h1>
-      <Images />
+      <main>
+        <header className="hero">
+          <StaticImage 
+            src='../assets/images/main.jpeg' 
+            alt='eggs' 
+            className="hero-img" 
+            placeholder="tracedSVG" 
+            layout='fullWidth' />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simply recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div> 
+        </header>
+      </main>
     </Layout>
     )
 }
