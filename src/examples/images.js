e62ae770058651851e26a9497998afbfff056c6e
Jeffrey <jglee1@live.com>
2022-08-06 10:31:24 -0700

    Tested gatsby plugin image props

--- src/examples/images.js
+++ src/examples/images.js
@@ -1,21 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
-import big from '../assets/images/big.jpg';
 import { StaticImage } from 'gatsby-plugin-image';
 
 const Images = () => {
   return (
     <Wrapper>
-      <StaticImage src={big}
-      alt='food' />
-      <h2>gatsby image</h2>
+      <article>
+        <h4>constrained / default</h4>
+        <StaticImage 
+          src='../assets/images/recipe-1.jpeg' 
+          alt='food'
+          placeholder='tracedSVG'
+          layout='constrained'
+          className='example-img'
+          as='section' />
+      </article>
+
+      <article>
+        <h4>fixed</h4>
+        <StaticImage 
+          src='../assets/images/recipe-1.jpeg' 
+          alt='food'
+          placeholder='blurred'
+          layout='fixed'
+          width={200}
+          className='example-img'
+          as='div' />
+      </article>
+
+      <article>
+        <h4>full-width</h4>
+        <StaticImage 
+          src='../assets/images/recipe-1.jpeg' 
+          alt='food'
+          placeholder='dominantColor'
+          layout='full-width'
+          className='example-img'
+          as='article' />
+      </article>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  img {
-    width: 200px;
+  width: 70vw;
+  margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
+  article {
+    border: 2px solid red;
+  }
+
+  .example-img {
+    border-radius: 1rem;
+  }
+
+  @media (min-width: 992px){
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `
 
