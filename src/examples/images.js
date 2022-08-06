3b7e6e0e24d8d6a37aece0fccfd5489fd856fc0f
Jeffrey <jglee1@live.com>
2022-08-05 23:34:42 -0700

    Added images to folder

---
+++ src/examples/images.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import big from '../assets/images/big.jpg';
+import { StaticImage } from 'gatsby-plugin-image';
+
+const Images = () => {
+  return (
+    <Wrapper>
+      <StaticImage src={big}
+      alt='food' />
+      <h2>gatsby image</h2>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  img {
+    width: 200px;
+  }
+`
+
+export default Images;
